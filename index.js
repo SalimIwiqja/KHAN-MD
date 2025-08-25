be5e6d3319505e00bf1a6acce625e967d6752d4b
SalimIwiqja <salimemaataoui@gmail.com>
2025-08-25 23:49:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-// deploy repo is closed no longer available for any type of deployment 👀
 
-// Deploy From New Repository ✅ 
-
-// Aeson-MD version 1.0 ❤️
-
-// Thanks For Chosing Us 🔥
-
-// Made with ♥️ By Salim 
-
-// Powered By Salim AESON-MD 2025 🚀
